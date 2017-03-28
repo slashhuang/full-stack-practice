3749de0dcd8923b97b3540ba1dc59fcb07ac9345
slashhuang <627284482@qq.com>
2017-03-28 15:19:12 +0800

    walk through http

---
+++ events/events.js
@@ -0,0 +1,8 @@
+/**
+ * Much of the Node.js core API is built around
+ * an idiomatic asynchronous event-driven architecture
+ * in which certain kinds of objects (called "emitters") periodically emit named events
+ * that cause Function objects ("listeners") to be called.
+ * @Author slashhuang
+ * 17/3/28
+ */
--- http/request.js
+++ http/request.js
@@ -6,3 +6,47 @@
  * payload:https://en.wikipedia.org/wiki/Payload_(computing)
  */
 
+/*
+ *class: http.Server类
+  http.createServer([requestListener])
+  Returns: {http.Server}
+  The requestListener is a function
+  which is automatically added to the 'request' event.
+  回调函数自动添加进request事件回调
+ */
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end();
+});
+//If a client connection emits an 'error' event, it will be forwarded here
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+/*
+ * When a new TCP stream is established.
+ * socket is an object of type net.Socket
+ */
+server.on('connection', socket => {
+    console.log(`connection comes in`)
+});
+/*
+ * request {http.IncomingMessage}
+ * response {http.ServerResponse}
+ * Emitted each time there is a request.
+ * Note that there may be multiple requests per connection
+ */
+server.on('request',(request,response) => {
+    let {url,method} = request;
+    console.log(`request metadata is ${method} ${url} `);
+    // close server
+    server.close(()=>process.stdout.write('server closed'));
+});
+server.listen(8000);
+
+
+
+
+
+
+
+
